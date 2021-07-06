392b72106b905f38a5be08afa786a190b782ee98
bryan-ubl <bryan.19421038@student.ubl.ac.id>
2021-07-06 20:17:57 +0700

    pertemuan 8

--- controller/User.js
+++ controller/User.js
@@ -1,63 +1,76 @@
-const userModel= require('../model/User')
-const bcrypt = require('bcrypt')
-exports.register = (data) =>
-    new Promise ((resolve, reject)=>{
-        //cari data
-        userModel.findOne({
-            uname: data.uname
-        }).then(user => {
-            if(user) {
-                resolve({
-                    status : false,
-                    pesan: 'dah ada'
-                })
-            }else{
-                bcrypt.hash(data.password, 10 , (err, hash) => {
-                  data.password = hash
+const kegiatanModel = require('../model/kegiatan')
+const objectid = require('mongoose').Types.ObjectId
 
-                  //input data
-                  userModel.create(data)
-                    .then (()=>{
-                        //console.log('berhasil')
-                        })
-                        resolve({
-                          status : true,
-                          pesan : 'berhasil'
-                    }).catch(() =>{
-                        //console.log('gagal')
-                        reject ({
-                            status : false,
-                            pesan : 'gagal'
-                        })
-                    })
-                })
-            }
+exports.create= (data) =>
+  new Promise((resolve, reject)=> {
+    //kegiatanModel.create() fungsi untuk menyimpan kegiatan yang di imput
+    kegiatanModel.create(data)
+      .then(()=> resolve({
+        status : true,
+        pesan : 'berhasil iput'
+      })).catch(()=> ({
+        status : false,
+        pesan : 'gagal iput'
+      }))
+  })
+
+  exports.showAllData = () =>
+    new Promise((resolve, reject)=>{
+      kegiatanModel.find()
+      .then (result =>{
+        resolve({
+          status : true,
+          pesan : 'berhasil menampilkan data',
+          data : result
         })
-
+      }).catch(()=> reject ({
+        status : false,
+        pesan : 'gagal menampilkan data',
+        data : []
+      }))
     })
 
-    exports.login = (data) =>
-      new Promise((resolve, reject)=>{
-        userModel.findOne({
-          uname: data.uname
-        }).then(user =>{
-          if(user){
-            if(bcrypt.compareSync(data.password, user.password)){
-              resolve({
-                status : true,
-                pesan : 'berhasil login'
-              })
-            }else{
-              reject ({
-                  status : false,
-                  pesan : 'username atau password salah'
-              })
-            }
-          }else{
-            reject ({
-                status : false,
-                pesan : 'username atau password salah'
-              })
-          }
+    exports.showbyID = (id) =>
+      new Promise((resolve, reject) => {
+        kegiatanModel.findOne({
+          _id: objectid(id)
         })
+        .then (result =>{
+          resolve({
+            status : true,
+            pesan : 'berhasil menampilkan data',
+            data : result
+          })
+        })
+        .catch(()=> reject ({
+          status : false,
+          pesan : 'gagal menampilkan data',
+          data : null
+        }))
       })
+
+exports.update = (id, data)=>
+  new Promise((resolve, reject)=>{
+    kegiatanModel.updateOne({
+      _id: objectid(id)
+    }, data).then (() => resolve({
+        status : true,
+        pesan : 'berhasil update data',
+      })).catch(()=> reject ({
+      status : false,
+      pesan : 'gagal update data',
+    }))
+  })
+
+exports.delete = (id) =>
+  new Promise((resolve, reject)=>{
+    kegiatanModel.deleteOne({
+      _id: objectid(id)
+    }).then (() => resolve({
+      status : true,
+      pesan : 'berhasil delete data',
+      })).catch(()=> reject ({
+      status : false,
+      pesan : 'gagal delete data',
+      }))
+  })
--- routes/kegiatan.js
+++ routes/kegiatan.js
@@ -6,4 +6,28 @@ router.post('/insert', (req, res)=> {
       .then(result =>res.json(result))
       .catch(err => res.json(err))
 })
+
+router.get('/getall', (req, res)=> {
+  kegiatanController.showAllData()
+      .then(result =>res.json(result))
+      .catch(err => res.json(err))
+})
+// reg params id
+router.get('/getid/:id', (req, res)=> {
+  kegiatanController.showbyID(req.params.id)
+      .then(result =>res.json(result))
+      .catch(err => res.json(err))
+})
+
+router.put('/update/:id', (req, res)=> {
+  kegiatanController.update(req.params.id, req.body)
+      .then(result =>res.json(result))
+      .catch(err => res.json(err))
+})
+
+router.delete('/delete/:id', (req, res)=> {
+  kegiatanController.delete(req.params.id)
+      .then(result =>res.json(result))
+      .catch(err => res.json(err))
+})
 module.exports = router
